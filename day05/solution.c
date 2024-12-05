7f032b50008ba3ce5e7f8e165ded7576208a821f
Bagrat Gobedashvili <gobedashvilibagrat@gmail.com>
2024-12-05 22:40:57 +0100

    day 5 star 2

--- day05/solution.c
+++ day05/solution.c
@@ -7,67 +7,17 @@ struct Rule {
   int right;
 };
 
-int main() {
-  FILE *file = fopen("./day05/input", "r");
 
-  fseek(file, 0, SEEK_END);
-  long fileSize = ftell(file);
-  rewind(file);
-
-  char *content = malloc(fileSize + 1);
-  fread(content, 1, fileSize, file);
-
-  //  char line[256];
-  //  while (fgets(line, sizeof(line), file)){
-  //    if (strchr(line, '|')) {
-  //
-  //      printf("%s", line);
-  //    }
-  //  }
-
-  //  split_by_crlf(content);
-  // split by 2 new lines
-  // first part rules, second pages
-  // go over each line in pages
-  // get a number
-  // find all rules where this number is last
-  // check if encountered number conteins what was first
-  // add to encountered
-
-  char *delim = "\r\n";
-  char *token = strtok(content, delim);
-
-  struct Rule *rules = NULL;
-  size_t rule_count = 0;
-
-  int super_res = 0;
-  while (token != NULL) {
-    if (strchr(token, '|')) {
-      int small, big;
-      if (sscanf(token, "%d|%d", &small, &big) != 2) {
-        fprintf(stderr, "Error: Invalid line format: %s", token);
-        continue;
-      }
-      rules = realloc(rules, (rule_count + 1) * sizeof(struct Rule));
-      rules[rule_count].right = big;
-      rules[rule_count].left = small;
-      rule_count += 1;
-    }
-
-    if (strchr(token, ',')) {
-      char *start = token;
-      printf("%s\n", start);
+int getMiddleNumberStar1(char *start, struct Rule *rules, size_t rule_count) {
       int *already_met = NULL;
       size_t already_met_count = 0;
 
       int broken = 0;
-      // printf("Segment: %s\n", lol);
       while (*start != '\0' && !broken) {
         char tmp[2];
         memcpy(tmp, start, 2);
         int num = atoi(tmp);
 
-        printf("::: %d\n", num);
         int i = 0;
         while (i <= rule_count) {
           if (rules[i].left == num) {
@@ -75,7 +25,6 @@ int main() {
             while (u < already_met_count) {
               if (already_met[u] == rules[i].right) {
                 broken = 1;
-                printf("broken %d\n", already_met[u]);
                 break;
               }
               u += 1;
@@ -91,23 +40,119 @@ int main() {
         if (*(start + 2) == '\0') {
           break;
         }
-        printf("--%s--\n", start);
         start += 3;
       }
 
+      int a = 0;
       if (!broken) {
-        int a = already_met[already_met_count / 2];
-
-
-        super_res += a;
+        a = already_met[already_met_count / 2];
       }
 
       free(already_met);
+      return a;
+}
+
+int getMiddleNumberStar2(char *line, struct Rule *rules, size_t rule_count) {
+      int *already_met = NULL;
+      size_t already_met_count = 0;
+      int pos = 0;
+
+      int broken = 0;
+      while (line[pos] != '\0') {
+        char tmp[2];
+        memcpy(tmp, &line[pos], 2);
+        int num = atoi(tmp);
+
+        int i = 0;
+        while (i <= rule_count) {
+          if (rules[i].left == num) {
+            int u = 0;
+            while (u < already_met_count) {
+              if (already_met[u] == rules[i].right) {
+                char tmp = line[u*3];
+                line[u*3] = line[pos];
+                line[pos] = tmp;
+                tmp = line[u*3 + 1];
+                line[u*3+1] = line[pos+1];
+                line[pos+1] = tmp;
+
+                broken = 1;
+                pos = 0;
+                already_met = NULL;
+                already_met_count = 0;
+                break;
+              }
+              u += 1;
+            }
+          }
+          i += 1;
+        }
+        already_met =
+            realloc(already_met, (already_met_count + 1) * sizeof(int));
+        already_met[already_met_count] = num;
+        already_met_count += 1;
+
+        if (line[pos + 2] == '\0') {
+          break;
+        }
+        pos += 3;
+      }
+
+      int a = 0;
+      if (broken) {
+        a = already_met[already_met_count / 2];
+      }
+
+      free(already_met);
+      return a;
+}
+
+int main() {
+  FILE *file = fopen("./day05/example", "r");
+
+  fseek(file, 0, SEEK_END);
+  long fileSize = ftell(file);
+  rewind(file);
+
+  char *content = malloc(fileSize + 1);
+  fread(content, 1, fileSize, file);
+
+  char *delim = "\r\n";
+  char *token = strtok(content, delim);
+
+  struct Rule *rules = NULL;
+  size_t rule_count = 0;
+
+  int star1 = 0;
+  int star2 = 0;
+  while (token != NULL) {
+    if (strchr(token, '|')) {
+      int small, big;
+      if (sscanf(token, "%d|%d", &small, &big) != 2) {
+        fprintf(stderr, "Error: Invalid line format: %s", token);
+        continue;
+      }
+      rules = realloc(rules, (rule_count + 1) * sizeof(struct Rule));
+      rules[rule_count].right = big;
+      rules[rule_count].left = small;
+      rule_count += 1;
+    }
+
+    if (strchr(token, ',')) {
+      char *t1 = malloc(strlen(token) + 1);
+      memcpy(t1, token, strlen(token) + 1);
+      star1 += getMiddleNumberStar1(t1, rules,  rule_count);
+      char *t2 = malloc(strlen(token) + 1);
+      memcpy(t2, token, strlen(token) + 1);
+      star2 += getMiddleNumberStar2(t2, rules,  rule_count);
+      free(t1);
+      free(t2);
     }
 
     token = strtok(NULL, delim);
   }
 
-  printf("%d", super_res);
+  printf("star1 %d\n", star1);
+  printf("star2 %d\n", star2);
   return EXIT_SUCCESS;
 }
