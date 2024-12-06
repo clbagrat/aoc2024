a9fef43e6825cd3574ab4be051bda9cbb91cfd30
Bagrat Gobedashvili <gobedashvilibagrat@gmail.com>
2024-12-06 23:33:42 +0100

    day 6 star 2

--- day06/solution.c
+++ day06/solution.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Record {
   int x;
@@ -47,36 +48,18 @@ void print_record(struct Record a) {
 
 void print_records(struct Record *all, int count) {
   for (int i = 0; i < count; i += 1) {
-    printf(":: x:%d y:%d dx:%d dy:%d meta:%d ::\n", all[i].x, all[i].y, all[i].dx,
-           all[i].dy, all[i].meta);
+    printf(":: x:%d y:%d dx:%d dy:%d meta:%d ::\n", all[i].x, all[i].y,
+           all[i].dx, all[i].dy, all[i].meta);
   }
 }
-//
-// void print(struct Point *obstales, int obstaclesCount, struct Point *visited,
-//            int visitedCount, int max) {
-//
-//   for (int i = 0; i < max * max; i += 1) {
-//     int x = i % (max);
-//     int y = i / (max);
-//     struct Point p = {.x = x, .y = y};
-//
-//     if (is_in_light(obstales, obstaclesCount, p)) {
-//       printf("#");
-//     } else if (is_in_light(visited, visitedCount, p)) {
-//       printf("X");
-//     } else {
-//       printf(".");
-//     }
-//     if (x == max - 1) {
-//       printf("\n");
-//     }
-//   }
-//   printf("\n\n");
-// }
-//
+void print_points(struct Point *all, int count) {
+  for (int i = 0; i < count; i += 1) {
+    printf(":: x:%d y:%d ::\n", all[i].x, all[i].y);
+  }
+}
 
-void go(struct Point *obstales, int obstaclesCount, struct Record position,
-        int max, struct Record **log, int *logCount, int *isLooped) {
+int go(struct Point *obstales, int obstaclesCount, struct Record position,
+        int max, struct Record **log, int *logCount) {
   *logCount = 0;
   int logCapacity = 10;
 
@@ -84,16 +67,11 @@ void go(struct Point *obstales, int obstaclesCount, struct Record position,
     if (!is_in(*log, *logCount, position)) {
       if (*logCount >= logCapacity - 1) {
         logCapacity *= 2;
-        printf("alloc %d %lu\n", logCapacity,
-               logCapacity * sizeof(RECORD));
-        struct Record *temp =  realloc(*log, logCapacity * sizeof(RECORD));
+        struct Record *temp = realloc(*log, logCapacity * sizeof(RECORD));
 
         *log = temp;
-        printf("alloc %p \n", log);
-
       }
       position.meta = *logCount;
-      print_record(position);
       (*log)[*logCount].x = position.x;
       (*log)[*logCount].y = position.y;
       (*log)[*logCount].dx = position.dx;
@@ -101,8 +79,7 @@ void go(struct Point *obstales, int obstaclesCount, struct Record position,
       (*log)[*logCount].meta = position.meta;
       *logCount += 1;
     } else {
-      *isLooped = 1;
-      return;
+      return 1;
     }
 
     struct Record next = {.x = position.x + position.dx,
@@ -111,8 +88,7 @@ void go(struct Point *obstales, int obstaclesCount, struct Record position,
                           .dy = position.dy};
 
     if (next.x >= max || next.x < 0 || next.y >= max || next.y < 0) {
-      return;
-      break;
+      return 0;
     }
 
     struct Point nextPoint = {.x = next.x, .y = next.y};
@@ -127,31 +103,91 @@ void go(struct Point *obstales, int obstaclesCount, struct Record position,
       position = next;
     }
   }
+  return 0;
+}
 
-  // while (!isOut) {
-  //   if (!is_in(visited, *visitedCount, position)) {
-  //     visited[*visitedCount].x = position.x;
-  //     visited[*visitedCount].y = position.y;
-  //     visited[*visitedCount].dx = direction.x;
-  //     visited[*visitedCount].dy = direction.y;
-  //     *visitedCount += 1;
-  //   }
+RECORD find_last(RECORD *log, int logCount, int x, int y) {
+  RECORD res = {.x = -1, .y = -1};
 
-  //   struct Point next = {.x = position.x + direction.x,
-  //                        .y = position.y + direction.y,
-  //                        .dx = position.dx,
-  //                        .dy = position.dy};
-  //   if (next.x >= max || next.x < 0 || next.y >= max || next.y < 0) {
-  //     isOut = 1;
-  //     break;
-  //   }
+  for (int i = 0; i < logCount; i += 1) {
+    if (log[i].x == x && log[i].y == y) {
+      res = log[i];
+    }
+  }
 
-  //   if (is_in(obstales, obstaclesCount, next)) {
-  //     rotate(&position);
-  //   } else {
-  //     position = next;
-  //   }
-  // }
+  return res;
+}
+
+void print_beautyful(RECORD *log, int logCount, struct Point *obstacles,
+                     int obstaclesCount, int max) {
+  struct Point *grid = malloc((max * max + 1) * sizeof(struct Point));
+
+  for (int i = 0; i < max * max; i += 1) {
+    if (i % (max) == 0) {
+      printf("\n");
+    }
+    int x = i % (max);
+    int y = i / (max);
+
+    RECORD lastLog = find_last(log, logCount, x, y);
+    struct Point p = {.x = x, .y = y};
+    if (is_in_points(obstacles, obstaclesCount, p)) {
+      if (x == obstacles[obstaclesCount - 1].x &&
+          y == obstacles[obstaclesCount - 1].y) {
+        printf("O");
+      } else {
+
+        printf("#");
+      }
+    } else if (lastLog.x >= 0) {
+      if (x == log[0].x && y == log[0].y) {
+
+        if (log[0].dy == -1) {
+          printf("^");
+        } else if (log[0].dy == 1) {
+          printf("v");
+        } else if (log[0].dx == 1) {
+          printf(">");
+        } else if (log[0].dx == -1) {
+          printf("<");
+        }
+
+      } else if (lastLog.dy == 0) {
+        printf("-");
+      } else if (lastLog.dx == 0) {
+        printf("|");
+      }
+    } else {
+      printf(".");
+    }
+  }
+  printf("\n");
+  printf("\n");
+}
+
+void star1(struct Point *obstales, int obstaclesCount,
+           struct Point startPosition, int max) {
+
+  struct Record position = {
+      .x = startPosition.x, .y = startPosition.y, .dx = 0, .dy = -1};
+  int isLooped = 0;
+  int logCount = 0;
+  struct Record *log = malloc(10 * sizeof(RECORD));
+
+  go(obstales, obstaclesCount, position, max, &log, &logCount);
+
+  struct Point *unique = malloc(max*max * sizeof( struct Point));
+  int uniqueCount = 0;
+
+  for (int i = 0; i < logCount; i += 1) {
+    struct Point p = {.x=log[i].x, .y = log[i].y};
+    if (!is_in_points(unique, uniqueCount, p)) {
+      unique[uniqueCount] = p;
+      uniqueCount += 1;
+    }
+  }
+
+  printf("star1: %d\n", uniqueCount);
 }
 
 void star2(struct Point *obstales, int obstaclesCount,
@@ -161,44 +197,52 @@ void star2(struct Point *obstales, int obstaclesCount,
       .x = startPosition.x, .y = startPosition.y, .dx = 0, .dy = -1};
   int isLooped = 0;
   int logCount = 0;
-  struct Record *log = (RECORD *)malloc(10 * sizeof(RECORD));
+  struct Record *log = malloc(10 * sizeof(RECORD));
 
-  printf("%p\n", &log);
-  go(obstales, obstaclesCount, position, max, &log, &logCount, &isLooped);
+  go(obstales, obstaclesCount, position, max, &log, &logCount);
+  //print_beautyful(log, logCount, obstales, obstaclesCount, max);
 
-  printf("%p\n", &log);
-  print_records(log, logCount);
+  int loops = 0;
+  struct Point *unique = malloc(max*max * sizeof( struct Point));
+  int uniqueCount = 0;
+  int copies = 0;
 
-  // struct Point *potential = malloc(obstaclesCount * sizeof(struct Point));
-  // int potentialCount = 0;
-  // get_all_visited(obstales, obstaclesCount, position, max, potential,
-  //                 &potentialCount);
+  for (int i = 1; i < logCount; i += 1) {
+    printf("%d/%d - %d  %d\n", i, logCount, loops, copies);
+    struct Point p = {.x=log[i].x, .y = log[i].y};
+    if (!is_in_points(unique, uniqueCount, p)) {
+      unique[uniqueCount] = p;
+      uniqueCount += 1;
+    } else {
+      copies += 1;
+      continue;
+    }
+    struct Point *newObstacles =
+        malloc((obstaclesCount + 1) * sizeof(struct Point));
+    memcpy(newObstacles, obstales, obstaclesCount * sizeof(struct Point));
+    newObstacles[obstaclesCount].x = log[i].x;
+    newObstacles[obstaclesCount].y = log[i].y;
 
-  // int loops = 0;
-  // for (int i = 1; i < potentialCount; i += 1) {
-  //   printf("%d %d \n", i, potentialCount);
-  //   struct Point *newObstacles =
-  //       malloc((obstaclesCount + 1) * sizeof(struct Point));
-  //   memcpy(newObstacles, obstales, obstaclesCount * sizeof(struct Point));
-  //   newObstacles[obstaclesCount].x = potential[i].x;
-  //   newObstacles[obstaclesCount].y = potential[i].y;
+    int isL = 0;
+    int newLogCount = 0;
+    struct Record *newLog = malloc(10 * sizeof(RECORD));
+    isL = go(newObstacles, obstaclesCount + 1, log[i-1], max, &newLog, &newLogCount);
+    if (isL && 0) {
+      print_beautyful(newLog, newLogCount, newObstacles, obstaclesCount + 1,
+                      max);
+    }
 
-  //   loops += is_looped(newObstacles, obstaclesCount + 1, potential[i - 1],
-  //   max); free(newObstacles);
-  // }
+    loops += isL;
+    free(newObstacles);
+    free(newLog);
+  }
 
-  // printf("star 2: %d\n", loops);
-  // free(potential);
+  printf("%d \n", loops);
 }
 
 int main() {
 
- // struct Record *log = (RECORD *)malloc(10 * sizeof(RECORD));
- // struct Record *log2 = (RECORD *)malloc(10 * sizeof(RECORD));
- // log[9].meta = 1;
- // print_records(log, 10);
- // return EXIT_SUCCESS;
-  FILE *file = fopen("./day06/example", "r");
+  FILE *file = fopen("./day06/input", "r");
 
   fseek(file, 0, SEEK_END);
   long fileSize = ftell(file);
@@ -206,6 +250,8 @@ int main() {
 
   char *content = malloc(fileSize + 1);
   fread(content, 1, fileSize, file);
+  fclose(file);
+
 
   struct Point *obstacles = malloc(fileSize * sizeof(struct Point));
   struct Point position;
@@ -238,6 +284,6 @@ int main() {
 
   // print_points(obstacles, obstaclesCount);
 
-  // star1(obstacles, obstaclesCount, position, max);
   star2(obstacles, obstaclesCount, position, max);
+  star1(obstacles, obstaclesCount, position, max);
 }
