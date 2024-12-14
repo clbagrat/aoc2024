e54baa73d7a96788b3c47df8ace17b2e12219994
Bagrat Gobedashvili <gobedashvilibagrat@gmail.com>
2024-12-14 16:07:12 +0100

    day14 star2

--- day14/solution.c
+++ day14/solution.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct point  {
+  int x;
+  int y;
+} point;
 
 int pmod(int a, int b) { return ((a % b) + b) % b; }
+
+int ptoi(int x, int y, int max) {
+  return y * max + x;
+}
 
 int main() {
   FILE *file = fopen("./day14/input", "r");
@@ -16,6 +26,9 @@ int main() {
   char line[256];
   int seconds = 100;
   int res[4] = {0, 0, 0, 0};
+  int robox[500][10000];
+  int roboy[500][10000];
+  int count = 0;
   while (fgets(line, sizeof(line), file)) {
     int x;
     int y;
@@ -23,6 +36,12 @@ int main() {
     int vy;
 
     sscanf(line, "p=%d,%d v=%d,%d", &x, &y, &vx, &vy);
+    for (int s = 0; s < 10000; s += 1) {
+      robox[count][s] = pmod(x + vx * s, w);
+      roboy[count][s] = pmod(y + vy * s, h);
+    }
+    count += 1;
+
     int nx = pmod(x + vx * seconds, w);
     int ny = pmod(y + vy * seconds, h);
     char b[3] = {'0', '0', '\0'};
@@ -45,6 +64,19 @@ int main() {
   for (int i = 0; i < 4; i += 1) {
     sum *= res[i];
   }
+
+  for (int i = 0; i < 10000; i += 1) {
+    char str [103*101 + 1];
+    memset(str, '.', 103*101); 
+    str[103*101] = '\0';
+    for (int u = 0; u < 500; u += 1) {
+      str[ptoi(robox[u][i], roboy[u][i], 101)] = '#';
+    }
+    if (strstr(str, "########") != NULL) {
+      printf("tree %d\n", i);
+    }
+  }
+
 
   printf("sum %d\n", sum);
   return EXIT_SUCCESS;
